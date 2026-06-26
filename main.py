c190e2fba38292b3d3be7bc527b8581316192ad2
Ivan name <ivan.myskov.89@mail.ru>
2026-06-26 19:41:00 +0300

    Исправлен URL: habr.com/ru/all/

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from datetime import datetime
 # ============================================================
 
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-URL = 'https://habr.com/ru/articles/'
+URL = 'https://habr.com/ru/all/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
 }
